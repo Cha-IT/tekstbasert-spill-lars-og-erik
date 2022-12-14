ebff4b62b0d581b925ea14e7085ccbb61056b46e
LarsPLoge <larsloge9@gmail.com>
2022-12-14 13:39:30 +0100

    Update

--- Dreamhold lignende spill.py
+++ Dreamhold lignende spill.py
@@ -1,16 +1,27 @@
 import random as rd
 startrom = "Soverom"
-playerinput = input("Hva gjør du?\n skriv hær: ")
-
+print("Du våkner og ser deg rundt i romme ditt. Du kler på deg og står opp fra senga. Skriv help(Sener) eller (Spiller) for å få hjelp om hva du kan gjøre videre.")
 class Spiller:
-    def __init__(self,poi = {}):
+    """Skriv interact også tingen du vil interacte med og den vil gi den en beskrivelse av objecte"""
+    def __init__(self,nåverendeRom,poi = {}):
         self.poi = poi
+        self.nåverendreRom = nåverendeRom
+
         """Her finner man hjelp til hvordan man spiller"""
     def interact(self, poi):
         self.poi = poi
+    def go(self):
+        for i in naborom[nåverendeRom]:
+            if i.value()==type(str):
+                print("hallo")
+            
+        self.nåverendrerom =  self.nåverendreRom.naborom[retning]
+
 
 
 class Sener:
+    """Skriv visInfo så vil det skrives ut alle tingene du kan gjøre noe med på rommet.
+    Skriv Go også en rettning n,w,s,e for å bevege deg til en dør."""
     def __init__(self,poi={},naborom = {}):
         self.poi = poi
         self.naborom = naborom 
@@ -21,12 +32,7 @@ class Sener:
     def adpois(self, dic):
         for keys,beskrivelse in dic.items():
             self.adpoi(keys,beskrivelse) 
-    def visInfo(self):
-        print(self.poi)
-
-    def go(self, rettning, naborom ={}):
-        self.rettning = rettning
-        self.naborom = naborom
+        
         
     def visRom(self, poi={}):
         self.poi = poi
@@ -41,20 +47,27 @@ soverompoi = {
     
 }
 stuepoi = {
-    "TV":"Besrivelse av hva som er på tv-en",
-    "Stuebord":"Besrivelse av hva som ligger på stuebordet",
-    "Terning":"Beskrivelse av terning på stuebordet"
+    "TV":"Besrivelse av hva som er på tv-en.",
+    "Stuebord":"Besrivelse av hva som ligger på stuebordet.",
+    "Terning":"Beskrivelse av terning på stuebordet.",
+    "Dør mot nord":"Denne døren går inn til soverommet.",
+    "Dør mot øst":"Denne døren går inn til kjøkkenet.",
+    "Dør mot sør":"Denne døren går inn til det mørke kottet."
+    "Dør mot vest":"Denne døren går inn til badet."
 }
 kjøkkenpoi ={
     "Glass med melk":"Det står et halvfult glass med melk på kjøkkenbenken. Uvist om hvor lenge den har stått der",
     "Brød":"Brød",
+    "Dør mot vest":"Denne døren tar deg inn tilbake til stuen."
 }
 badpoi = {
     "Tannbørste":"Det står en rosa jordan tannbørste oppi en gul kopp. Den ser litt sliten ut.",
-    "Tannkrem":"Tannkremen ser helt flat ut. Får se om jeg klarer og presse ut for nokk til en dag til."
+    "Tannkrem":"Tannkremen ser helt flat ut. Får se om jeg klarer og presse ut for nokk til en dag til.",
+    "Dør til øst":"Denne døren tar deg inn tilbake til stuen"
 }
 kottpoi = {
-    "Gnom":"Det står en gnom mitt på rommet. Hvorfor står det en gnom midt på rommet. Den ser ikke snill ut."
+    "Gnom":"Det står en gnom mitt på rommet. Hvorfor står det en gnom midt på rommet. Den ser ikke snill ut.",
+    "Dør til nord":"Denne døre går inn tilbake til stuen."
 }
 
 soverom = Sener(soverompoi)
@@ -70,7 +83,7 @@ stuerom = {
     "s":kott,
     "w":bad
 }
-soverom = {
+soveromrom = {
     "n":None,
     "e":None,
     "s":stue,
@@ -94,6 +107,12 @@ badrom = {
     "s":None,
     "w":None
 }
+bad.addnaboRom(badrom)
+stue.addnaboRom(stue)
+soveromrom.adnaboRom(soverom)
+kjøkken.addnaboRom(kjøkken)
+kott.addnaboRom(kott)
+
 #Fiender?
 class gnom(object):     #Ikke bruk class.... sier hans
     name = "Gnomeo"
