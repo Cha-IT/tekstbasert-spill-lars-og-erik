b3c78ceb1776167249a42380ce4fa4a1b7b9f140
Erik Lein <119315608+Ereeek@users.noreply.github.com>
2022-12-15 08:27:50 +0100

    Update Dreamhold lignende spill.py

--- Dreamhold lignende spill.py
+++ Dreamhold lignende spill.py
@@ -171,48 +171,6 @@ def gnomhelse():
     if gnom.health <= 0:
         print("Gnomeo pines og venter en smertefull og ikke heroisk død.\n LOL.\n Han er ley seg og vil hjem til hans mor og beelskede Juliet - Gnomeo og Juliet")
 
-"""def attack():
-    print("Velg ditt angrep")
-    valg = input("1. Normal attack \n2. Special attack \n3. Super attack \n (25% Success rate)")
-    angrip = {"1":10,
-        "2":20,       
-        "3":50,
-        "0":0}
-    if valg == "1":
-        angrep = "Normal attack"
-        print("Du slår fienden")
-        print(f"du bruker et {angrep}, fienden mister {angrip[valg]} liv og har igjen {gnom.health - angrip[valg]} liv")
-        enemy.health = enemy.health - angrip[valg]
-        gnomhelse()
-
-    elif attack == "2":
-        angrep = "special attack"
-        print("Du sparker fienden")
-        print(f"du bruker et {angrep}, fienden mister {angrip[valg]} liv og har igjen {gnom.health - angrip[valg]} liv")
-        enemy.health = enemy.health - angrip[valg]
-        gnomhelse()
-
-    elif attack == "3":
-        angrep = "super attack"
-        print("Du løper mot fienden for å angripe alt du kan")
-        sjanse = rd.randint(1,4)
-        if sjanse != 4:
-            print("Du slår og sparker fienden med en syk kombo")
-            print(f"fienden mister 50 liv og har igjen {gnom.health - 50} liv")
-            enemy.health = enemy.health - angrip[valg]
-            gnomhelse()
-        else:
-            print("du dreit deg ut")
-            valg = 0
-            enemy.health = enemy.health - angrip[valg]
-            gnomhelse()
-
-    else:
-        print("velg alternativ 1, 2 eller 3")
-        attack()
-if enemy.health <= 0: #Spesifisere fienden (gnom)
-    print("Gnomeo pines og venter en smertefull og ikke heroisk død.\n LOL.\n Han er ley seg og vil hjem til hans mor og beelskede Juliet - Gnomeo og Juliet")
-"""
 def attack():
     while gnom.health > 0:
         print("Velg ditt angrep")
